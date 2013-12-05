23b00dbbe4cf1e73540dc6924a433516714b5b02
November Foxtrot <november@fox.trot>
2013-12-05 23:47:06 +0900

    more

--- helper_test.go
+++ helper_test.go
@@ -17,7 +17,26 @@ func TestSTR(t *testing.T) {
 	for _, test := range tests {
 		actual := str(test.in)
 		if actual != test.out {
-			t.Errorf("expected %v actual %v", test.in, test.out)
+			t.Errorf("expected %v actual %v", test.in, actual)
+		}
+	}
+}
+
+func TestJOIN(t *testing.T) {
+	tests := []struct {
+		left  string
+		right string
+		out string
+	}{
+		{"Dude", "Dude", "Dude/Dude"},
+		{"/Dude", "/Dude", "/Dude/Dude"},
+		{"/Dude", "Dude", "/Dude/Dude"},
+	}
+
+	for _, test := range tests {
+		actual := join(test.left, test.right)
+		if actual != test.out {
+			t.Errorf("expected %s actual %s", test.out, actual)
 		}
 	}
 }
