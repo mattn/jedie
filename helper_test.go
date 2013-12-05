991dde1e738a6c7573635bee32850f94a585f52e
November Foxtrot <november@fox.trot>
2013-12-05 23:40:13 +0900

    one down

---
+++ helper_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSTR(t *testing.T) {
+	tests := []struct {
+		in  interface{}
+		out interface{}
+	}{
+		{"", ""},
+		{1, ""},
+		{"Dude", "Dude"},
+	}
+
+	for _, test := range tests {
+		actual := str(test.in)
+		if actual != test.out {
+			t.Errorf("expected %V actual %V", test.in, test.out)
+		}
+	}
+}
