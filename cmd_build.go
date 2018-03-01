7d41096e6e646c8497dc998aa641af647e3ee3a4
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-03-01 16:33:12 +0900

    -s -d for build command

--- cmd_build.go
+++ cmd_build.go
@@ -13,7 +13,23 @@ func init() {
 			if err := cfg.load("_config.yml"); err != nil {
 				return err
 			}
+			if c.String("s") != "" {
+				cfg.Source = c.String("s")
+			}
+			if c.String("d") != "" {
+				cfg.Destination = c.String("d")
+			}
 			return cfg.Build()
+		},
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "s",
+				Usage: "source path",
+			},
+			cli.StringFlag{
+				Name:  "d",
+				Usage: "destination path",
+			},
 		},
 	})
 }
